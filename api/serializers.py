c8c470efdad774e73546da135c16833cde39170a
rings-s <rings9619@gmail.com>
2024-10-15 19:48:22 -0400

    explaining  serializers methods && nested serializers  >> Focus on it

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,7 @@
 from rest_framework import serializers
 from .models import Product, Order, OrderItem
 
-
-
+# ProductSerializer: Handles serialization for the Product model.
 class ProductSerializer(serializers.ModelSerializer):
     
     class Meta:
@@ -12,36 +11,41 @@ class ProductSerializer(serializers.ModelSerializer):
             'price',
             'description',
             'stock',
-        
         )
+
+    # validate_price: Custom validator to ensure that the price is greater than zero.
     def validate_price(self, value):
         if value <= 0:
             raise serializers.ValidationError("Price must be greater than zero.")
         return value
 
 
+# OrderItemSerializer: Handles serialization for OrderItem model.
 class OrderItemSerializer(serializers.ModelSerializer):
-    # product = ProductSerializer(read_only=True)
+    # Fetching and displaying product details through nested data.
+    # product = ProductSerializer(read_only=True)  # Example of full product serialization commented out.
     product_name = serializers.CharField(source='product.name')
     product_price = serializers.DecimalField(source='product.price', max_digits=10, decimal_places=2)
+    
     class Meta:
         model = OrderItem
         fields = (
-            # 'product',
+            # 'product',  # Uncomment to serialize full product details.
             'product_name',
             'product_price',
             'quantity',
             'item_subtotal',
-            
         )
 
-# nested serializer
+# OrderSerializer: Handles serialization for the Order model with nested serialization for order items.
+
 class OrderSerializer(serializers.ModelSerializer):
-    # nested serializer
-    items = OrderItemSerializer(many=True, read_only=True) 
+    items = OrderItemSerializer(many=True, read_only=True) # Nested serializer for handling multiple OrderItems linked to an Order.
+
     total_price = serializers.SerializerMethodField()
     
     def get_total_price(self, obj):
+        # Calculate total price of the order by summing up all the item subtotals.
         order_items = obj.items.all()
         return sum(order_item.item_subtotal for order_item in order_items)
     
@@ -54,11 +58,7 @@ class OrderSerializer(serializers.ModelSerializer):
             'status',
             'items',
             'total_price',
-            )
-        
-        
-
-
+        )
 
 
     # serializers convert python data types into json data
